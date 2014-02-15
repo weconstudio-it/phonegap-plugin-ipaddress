3b29d00e08292778d53fbda14eaf9ff2e0aea2be
Alessandro Cabutto <alessandro.cabutto@gmail.com>
2014-02-15 13:06:43 +0100

    filtered IPv4 addresses only

--- src/it/weconstudio/phonegap/plugins/android/IpAddress.java
+++ src/it/weconstudio/phonegap/plugins/android/IpAddress.java
@@ -1,4 +1,4 @@
-package it.weconstudio.phonegap.plugin.ipaddress;
+package it.weconstudio.phonegap.plugins.android;
  
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -11,35 +11,43 @@ import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.CordovaResourceApi.OpenForReadResult;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
  
 import android.util.Log;
- 
+
+/**
+ * IP Address detection plugin
+ * @author weconstudio
+ */
 public class IpAddress extends CordovaPlugin {
  
-    //public PluginResult execute(String action, JSONArray args, String callbackId) {
 	@Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    	return true;
-        //if (action.equals("get")) {
-        	//return new PluginResult(PluginResult.Status.OK, "1111");
-            //String ipAddress = getIpAddress();
-            //if (ipAddress != null && ipAddress.length() > 0) {
-            //    return new PluginResult(PluginResult.Status.OK, ipAddress);
-            //} else {
-            //    return new PluginResult(PluginResult.Status.ERROR);
-            //}
-        //} else {
-         //   return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        //}
+    	
+    	String ipAddress = getIpAddress();
+        if (ipAddress != null && ipAddress.length() > 0) {
+            callbackContext.success(ipAddress);
+            
+            return true;
+        } else {
+            callbackContext.error("Operation failed");
+            
+            return false;
+        }
     }
- 
+
+	/**
+	 * 
+	 * @return String Ip Address
+	 */
     private String getIpAddress() {
         try {
             for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
                 NetworkInterface intf = en.nextElement();
                 for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
                     InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress()) {
+                    if (!inetAddress.isLoopbackAddress() && inetAddress.getClass().getName() == "java.net.Inet4Address") {
                         return inetAddress.getHostAddress().toString();
                     }
                 }
@@ -47,6 +55,7 @@ public class IpAddress extends CordovaPlugin {
         } catch (SocketException ex) {
             Log.e("IpAddress", ex.toString());
         }
+        
         return null;
     }
 }
